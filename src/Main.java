bfbc52830dcfae9d5d0eca5def937cc01dea2211
Zia999 <92579255+Zia999@users.noreply.github.com>
2022-11-06 21:42:07 +0000

    scanner

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your year of birth");
+        int yearOfBirth = scanner.nextInt();
+
+        scanner.nextLine(); //enter key separator
+
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        int age = 2022 - yearOfBirth;
+
+        System.out.println("your name is" + name +", and you are " + age + " years old." );
+
+        scanner.close();
 
     }
-}
+
+
+
+
+    }
 
 
